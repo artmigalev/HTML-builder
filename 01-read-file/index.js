f97470f7ac631de8137d0fd7a85f54c9672ffd15
artmigalev <art_migalev@yahoo.com>
2025-01-14 19:22:44 +0300

    fix:lint and prettier

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-
 const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
 let data = '';
 
 stream.on('data', (chunk) => console.log((data += chunk)));
 stream.on('end', () => console.log('End', data.length));
+
+
