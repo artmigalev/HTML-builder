7c7635e5c861e41adc70f94f79871916ed34bd5d
artmigalev <art_migalev@yahoo.com>
2025-01-16 22:36:34 +0300

    feat: 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -52,3 +52,22 @@ fs.readFile(tempPath, 'utf-8', (err, template) => {
     });
   });
 });
+const styles = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'styles.css'),
+);
+const pathToReadFolder = path.join(__dirname, 'styles');
+fs.readdir(pathToReadFolder, (err, files) => {
+  if (err) {
+    console.log('Errror!!!!!! Мать твою .... ');
+    return;
+  }
+  files.forEach((file) => {
+    const readFile = fs.createReadStream(
+      path.join(pathToReadFolder, file),
+      'utf-8',
+    );
+    readFile.on('data', (chunk) => {
+      styles.write(chunk);
+    });
+  });
+});
